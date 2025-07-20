dc6e9a40e7c6f1d38174585d45fdbc6248190dab
Ben Scarletti <scarletti.ben@gmail.com>
2025-07-20 21:03:28 +0100

    added simple testing for site

--- script.js
+++ script.js
@@ -8,6 +8,19 @@ const page = /** @type {HTMLDivElement} */
 const main = /** @type {HTMLDivElement} */
     (document.getElementById('main'));
 
+const originToggle = /** @type {HTMLInputElement} */
+    (document.getElementById('origin-toggle'));
+
+// < ======================================================
+// < Declarations
+// < ======================================================
+
+const SAME_ORIGIN_URL = 
+    '/api/test-cors';
+
+const CROSS_ORIGIN_URL = 
+    'https://vercel-cors-proxy-eight.vercel.app/api/test-cors';
+
 // ~ ======================================================
 // ~ Entry Point
 // ~ ======================================================
@@ -17,6 +30,38 @@ const main = /** @type {HTMLDivElement} */
  */
 (() => {
 
-    console.log('running');
+    const testers = document.querySelectorAll('.request-tester');
+    for (const tester of testers) {
+
+        const button = tester.querySelector('.request-button');
+        const output = tester.querySelector('.response-container');
+
+        button.addEventListener('click', async () => {
+            const method = button.textContent.trim();
+            try {
+                const path = originToggle.checked ?  CROSS_ORIGIN_URL : SAME_ORIGIN_URL;
+                const response = await fetch(path, { method });
+                const responseText = await response.text();
+                let data;
+                try {
+                    data = JSON.parse(responseText);
+                } catch {
+                    data = responseText;
+                }
+                output.textContent = JSON.stringify({
+                    status: response.status,
+                    statusText: response.statusText,
+                    data: data
+                }, null, 2);
+            } catch (error) {
+                output.textContent = JSON.stringify({
+                    name: error.name,
+                    message: error.message,
+                    stack: error.stack
+                }, null, 2);
+            }
+        });
+
+    }
 
 })();
