90d3851db4da5117b450f1a4930e316d8aa84a1b
Ben Scarletti <scarletti.ben@gmail.com>
2025-07-20 16:43:50 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Vercel CORS Proxy
+This `README` is a work in progress.
+
+# Project Metadata
+```yaml
+---
+title: "Vercel CORS Proxy"
+date: "2025-07-20"
+# last_modified_at: ""
+# description: ""
+categories: [
+  miscellaneous
+]
+tags: [
+  coding, dev, webdev, html, javascript, css, json, api, cors, cors proxy
+]
+---
+```
---
+++ script.js
@@ -0,0 +1,22 @@
+// < ======================================================
+// < Element Queries
+// < ======================================================
+
+const page = /** @type {HTMLDivElement} */
+    (document.getElementById('page'));
+
+const main = /** @type {HTMLDivElement} */
+    (document.getElementById('main'));
+
+// ~ ======================================================
+// ~ Entry Point
+// ~ ======================================================
+
+/**
+ * Entry point for the application (IIFE)
+ */
+(() => {
+
+    console.log('running');
+
+})();
