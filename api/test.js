ca5cd20b78b1e0088bec4ff58959e66abea1d413
Ben Scarletti <scarletti.ben@gmail.com>
2025-07-21 08:16:03 +0100

    cleaned up api/test endpoint and added CORS functionality

--- api/test.js
+++ api/test.js
@@ -9,30 +9,38 @@
  * @returns {void}
  */
 export default function handler(request, response) {
+
+    // Set CORS headers to allow cross-origin requests
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Methods', '*');
+    response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    // Handle an OPTIONS request (CORS preflight request)
+    if (request.method === 'OPTIONS') {
+        response.status(204).end();
+        return;
+    }
+
+    // Handle a GET request
     if (request.method === 'GET') {
         response.status(200).json({
             message: 'GET request received',
             data: request.query,
             timestamp: new Date(),
-            info: `GET requests to this endpoint are ceremonial`,
+            info: `GET requests to this endpoint are for testing`,
             ok: true
         });
-    } else if (request.method === 'POST') {
-        response.status(200).json({
-            message: `POST request received`,
-            data: request.body,
-            timestamp: new Date(),
-            info: `POST requests to this endpoint are ceremonial`,
-            ok: true
-        });
-    } else {
-        response.setHeader('Allow', 'GET, POST');
-        response.status(405).json({
-            message: `${request.method} request received`,
-            data: request.query,
-            timestamp: new Date(),
-            info: `${request.method} requests are not allowed`,
-            ok: false
-        });
+        return;
     }
+
+    // Handle all other requests with not implemented error
+    response.setHeader('Allow', 'GET, OPTIONS');
+    response.status(501).json({
+        message: `${request.method} request received`,
+        data: request.query,
+        timestamp: new Date(),
+        info: `${request.method} functionality not implemented`,
+        ok: false
+    });
+
 }
