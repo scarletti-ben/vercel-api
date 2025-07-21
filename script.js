333439cbbe54c4ebb189d0bbd18ad3f6382f165d
Ben Scarletti <scarletti.ben@gmail.com>
2025-07-21 14:15:28 +0100

    entirely removed base site testing functionality

--- script.js
+++ script.js
@@ -8,19 +8,6 @@ const page = /** @type {HTMLDivElement} */
 const main = /** @type {HTMLDivElement} */
     (document.getElementById('main'));
 
-const originToggle = /** @type {HTMLInputElement} */
-    (document.getElementById('origin-toggle'));
-
-// < ======================================================
-// < Declarations
-// < ======================================================
-
-const RELATIVE_URL = 
-    '/api/test-cors';
-
-const FULL_URL = 
-    'https://vercel-cors-proxy-eight.vercel.app/api/test-cors';
-
 // ~ ======================================================
 // ~ Entry Point
 // ~ ======================================================
@@ -30,38 +17,6 @@ const FULL_URL =
  */
 (() => {
 
-    const testers = document.querySelectorAll('.request-tester');
-    for (const tester of testers) {
-
-        const button = tester.querySelector('.request-button');
-        const output = tester.querySelector('.response-container');
-
-        button.addEventListener('click', async () => {
-            const method = button.textContent.trim();
-            try {
-                const path = originToggle.checked ?  RELATIVE_URL : FULL_URL;
-                const response = await fetch(path, { method });
-                const responseText = await response.text();
-                let data;
-                try {
-                    data = JSON.parse(responseText);
-                } catch {
-                    data = responseText;
-                }
-                output.textContent = JSON.stringify({
-                    status: response.status,
-                    statusText: response.statusText,
-                    data: data
-                }, null, 2);
-            } catch (error) {
-                output.textContent = JSON.stringify({
-                    name: error.name,
-                    message: error.message,
-                    stack: error.stack
-                }, null, 2);
-            }
-        });
-
-    }
+    console.log('running');
 
 })();
